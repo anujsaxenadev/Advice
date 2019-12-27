92b95d648155039c9f2a62e531fb2eab1135f263
Anuj Saxena <anuj.saxena.dev@gmail.com>
2019-12-27 16:30:25 +0530

    Validating response and Playing advice.

--- advice.py
+++ advice.py
@@ -2,8 +2,20 @@ import requests
 import os
 from gtts import gTTS
 
-# Getting responce from API from adviceslip.com
+# Getting response from API from adviceslip.com
 url = "https://api.adviceslip.com/advice"
 language = 'en'
 response = requests.get(url)
 
+# Validating response and Playing advice.
+if response:
+	json = response.json()
+	advice = json['slip']['advice']
+	print("Advice")
+	print(advice)
+	voice = gTTS(text=advice, lang=language, slow=False) 
+	voice.save("voice.mp3") 
+	os.system("mplayer voice.mp3")
+	os.remove("voice.mp3")
+else:
+    print('An error has occurred. Please check network your connection.')
