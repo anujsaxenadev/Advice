917dacbbb5e63335e736a766cac6d7babb1e0531
Anuj Saxena <anuj.saxena.dev@gmail.com>
2019-12-27 16:29:03 +0530

    Getting responce from API from adviceslip.com

--- advice.py
+++ advice.py
@@ -1,3 +1,9 @@
 import requests
 import os
 from gtts import gTTS
+
+# Getting responce from API from adviceslip.com
+url = "https://api.adviceslip.com/advice"
+language = 'en'
+response = requests.get(url)
+
